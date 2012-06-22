1f9e6b01853a7f23d20d49e95771352c9ff81698
socioecommerce2012 <socioecommerce2012@gmail.com>
2012-06-22 10:28:38 +0530

    With Mysqli Sample

--- socioEcommerceCommon/php/mysqli/mysqliPrepareSqlStatements.php
+++ socioEcommerceCommon/php/mysqli/mysqliPrepareSqlStatements.php
@@ -1,22 +1,85 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of mysqliPrepareSqlStatements
  *
  * @author kodakand
  */
 class mysqliPrepareSqlStatements {
-     
-    public static function getPreparedStatment($sql,$params){
-        
+
+    public static function getPreparedStatment($sql, $params, $mysqli) {
+
+//        $countOfCharacters = substr_count($sql, '?');
+//        $sizeOfArray = count($params);
+//        $firstParamOfPreparedStatement;
+//        $secondParamOfPreparedStatement;
+//
+//
+//
+//        if ($countOfCharacters != $sizeOfArray) {
+//            echo 'Count Of Parameters To Be Replaces Is Not Equal To The Params Present In Array';
+//            return null;
+//        }
+//
+//        if (!$preparedStatement = $mysqli->prepare($sql)) {
+//            
+//        }
+//        for ($i = 0; $i < count($params); $i++) {
+//
+//            if (is_string($params[$i])) {
+//                $firstParamOfPreparedStatement .= 's';
+//                $secondParamOfPreparedStatement .= $params[$i];
+//            } else {
+//                $firstParamOfPreparedStatement .= 'd';
+//
+//                $secondParamOfPreparedStatement .= $params[$i];
+//            }
+//        }
+//        echo $firstParamOfPreparedStatement . "\n";
+//        echo $secondParamOfPreparedStatement."\n";
+//        if (!$preparedStatement->bind_param($firstParamOfPreparedStatement, $secondParamOfPreparedStatement)) {
+//            echo "Binding parameters failed: (" . $preparedStatement->errno . ") " . $preparedStatement->error;
+//        }
+//        
+//        echo is_object($preparedStatement)."\n";
+//        if (!($res = $preparedStatement->get_result())) {
+//            echo "Getting result set failed: (" . $preparedStatement->errno . ") " . $preparedStatement->error;
+//        }
+//        
+//        var_dump($res->fetch_all());
+
+
+
+        /* Prepared statement, stage 1: prepare */
+        if (!($stmt = $mysqli->prepare("INSERT INTO interestUserMap(interestId,userId) VALUES (?,?)"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        /* Prepared statement, stage 2: bind and execute */
+        $id = 0;
+        if (!$stmt->bind_param("ii", $id,$id)) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        /* Prepared statement: repeated execution, only data transferred from client to server */
+        for ($id = 2; $id < 5; $id++) {
+            if (!$stmt->execute()) {
+                echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            }
+        }
+
+        /* explicit close recommended */
+        $stmt->close();
+
+        /* Non-prepared statement */
+        $res = $mysqli->query("SELECT interestId,userId FROM interestUserMap");
+        var_dump($res->fetch_all());
     }
-    
-    
+
 }
 
 ?>
--- socioEcommerceCommon/php/mysqli/mysqliUtitlities.php
+++ socioEcommerceCommon/php/mysqli/mysqliUtitlities.php
@@ -5,7 +5,7 @@
  * @author kodakand
  */
 include 'mysqliConnector.php';
-
+include 'mysqliPrepareSqlStatements.php';
 class mysqliUtitlities {
 
     private $connHandler;
@@ -16,26 +16,33 @@ class mysqliUtitlities {
         $this->connHandler = $this->mysqli->getConnection();
     }
 
-    public function executeQuery($sql) {
+    public function executeQuery($sql, $params) {
+        $preparedStatment = mysqliPrepareSqlStatements::getPreparedStatment($sql, $params, $this->connHandler);
         try {
-            if (!$sqlHandler = $this->connHandler->query("SELECT * FROM itemInfo LIMIT 10")) {
-                throw new Exception;
+//            if (!$sqlHandler = $this->connHandler->query("SELECT * FROM itemInfo LIMIT 10")) {
+//                throw new Exception;
+//            }
+//            return $sqlHandler;
+            if (!$preparedStatment->execute()) {
+                echo "Execute failed: (" . $preparedStatment->errno . ") " . $preparedStatment->error;
             }
-            return $sqlHandler;
+            $res = $preparedStatment->get_result();
+            $row = $preparedStatment->fetch_assoc();
+            print_r($res);
+            print_r($row);
         } catch (Exception $e) {
             die("Failed Executing The Given SQL statement");
         }
     }
-    
-    
-    
 
 }
 
 $mysqlUtilities = new mysqliUtitlities();
-$handler = $mysqlUtilities->executeQuery($sql);
+$sql = "Select * FROM itemInfo WHERE itemId > (?)";
+$params = array(5);
+$handler = $mysqlUtilities->executeQuery($sql,$params);
 
-while($row = $handler->fetch_object()){    // OBJECT RETURNS IN TERMS OF ASSOC ROWS RETURNS IN FORM OF  ARRAY
+while ($row = $handler->fetch_object()) {    // OBJECT RETURNS IN TERMS OF ASSOC ROWS RETURNS IN FORM OF  ARRAY
     print_r($row);
 }
 //
@@ -45,28 +52,28 @@ while($row = $handler->fetch_object()){    // OBJECT RETURNS IN TERMS OF ASSOC R
 //echo $handler->fetch
 //$row = $result->fetch_row();
 /*
- @ $db = new mysqli($dbhost, $un, $ps, $dbname);
-$query = "SELECT field1, field2 ".
-         "FROM table1 ".
-         "WHERE field1={$some_value}";
-$results = $db->query($query);
+  @ $db = new mysqli($dbhost, $un, $ps, $dbname);
+  $query = "SELECT field1, field2 ".
+  "FROM table1 ".
+  "WHERE field1={$some_value}";
+  $results = $db->query($query);
 
-while ($result = $results->fetch_object()) {
-    // do something with the results
-}
+  while ($result = $results->fetch_object()) {
+  // do something with the results
+  }
 
-$query = "SELECT field1, field2 ".
-         "FROM table2 ".
-         "WHERE field1={$some_value2}";
-// question 1
-$results = $db->query($query);
+  $query = "SELECT field1, field2 ".
+  "FROM table2 ".
+  "WHERE field1={$some_value2}";
+  // question 1
+  $results = $db->query($query);
 
-while ($result = $results->fetch_object()) {
-    // do something with the second set of results
-}
+  while ($result = $results->fetch_object()) {
+  // do something with the second set of results
+  }
 
-// tidy up, question 2
-if ($results) { $results->free(); }
-if ($db) { $db->close(); }
+  // tidy up, question 2
+  if ($results) { $results->free(); }
+  if ($db) { $db->close(); }
  */
 ?>
